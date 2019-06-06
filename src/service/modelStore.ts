316515376b00172dbc408d5bb809117b10920f7c
Deberson Paula <deberson.paula@gmail.com>
2019-06-06 14:02:11 -0300

    added services

--- src/service/modelStore.ts
+++ src/service/modelStore.ts
@@ -1,6 +1,8 @@
 import { ModelStore } from 'exredux';
+import { ProductListModel } from './models/ProductListModel';
+import { ProductItemModel } from './models/ProductItemModel';
 
 export const modelStore = new ModelStore({
   devExtension: process.env.NODE_ENV === 'development',
-  models: []
+  models: [ProductItemModel, ProductListModel]
 });
